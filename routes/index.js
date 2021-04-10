37244fc73adabeeef8492f61ab8d5e8370f1792f
Felipedearamburu <fdearamburu@udesa.edu.ar>
2021-04-10 15:38:04 -0300

    routes index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mercado Libre' });
 });
-
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Productos' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Registrate' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Inicia sesión' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Perfil del usuario' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Agrega tu producto' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Edita tu perfil' });
+});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Respuestas a tu busqueda' });
+});
 module.exports = router;
