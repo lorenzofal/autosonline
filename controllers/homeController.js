0b6a8e994061e2425303a63a56f08e018bdb42c3
lorenzofal <63105821+lorenzofal@users.noreply.github.com>
2021-04-13 00:18:34 +0300

    for en el ejs andando

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
-//const autos = require('../data/autos')
+const autos = require('../data/autos')
 module.exports = {
     index: (req, res)=>{
-        return res.render('index', {title: 'Pagina de Inicio' })
+        return res.render('index', {title: 'Pagina de Inicio', autos: autos.lista })
     }
 }
