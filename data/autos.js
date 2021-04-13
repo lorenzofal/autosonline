e70ffaf59a2453de80684ae208007a8e3dc7d6e8
lorenzofal <63105821+lorenzofal@users.noreply.github.com>
2021-04-13 07:39:05 +0300

    creados header y footer en distintos ejs
    todavia no hice el include desde el index porque no funciono

--- data/autos.js
+++ data/autos.js
@@ -3,26 +3,26 @@ let autos = {
         {
             id : 1,
             nombre : "Kia Rio",
-            precio : "$500.000",
-            imagen : "kia rio.jpeg"
+            precio : "500.000",
+            imagen : "../public/autosImg/golf.jpeg"
         },
         {
             id : 2,
             nombre : "bmw",
             precio : "1500.000",
-            imagen : "bmw.jpeg"
+            imagen : "../public/autosImg/golf.jpeg"
         },
         {
             id : 3,
             nombre : "etios",
-            precio : "$300.000",
-            imagen : "etios.jpeg"
+            precio : "300.000",
+            imagen : "../public/autosImg/golf.jpeg"
         },
         {
             id : 4,
             nombre : "golf",
-            precio : "$900.000",
-            imagen : "golf.jpeg"
+            precio : "900.000",
+            imagen : "../public/autosImg/golf.jpeg"
         }
 
     ]
